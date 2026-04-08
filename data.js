0502ed433b08b3957ccaccdad91ec39fa6b0ced7
nexuiagencycontact <nexuiagency.contact@gmail.com>
2026-04-08 15:27:03 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,11 @@
+const kanzData = {
+    fr: {
+        title: "Kanz",
+        subtitle: "Store",
+        slogan: "LE MEILLEUR DE LA NATURE, CHEZ VOUS.",
+        status: "SYSTÈME EN LIGNE",
+        ticker: "MIEL DE THYM DISPONIBLE // NOUVEAU ARRIVAGE : SIDR MALIKI // LIVRAISON ACTIVE",
+    }
+};
+
+
